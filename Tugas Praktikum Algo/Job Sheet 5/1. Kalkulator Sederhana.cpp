c5780a93b2270cd1e72f18599f83c84deed7ef24
Zyxcid <121550192+Zyxcid@users.noreply.github.com>
2023-10-22 00:03:05 +0700

    Add files via upload

---
+++ Tugas Praktikum Algo/Job Sheet 5/1. Kalkulator Sederhana.cpp
@@ -0,0 +1,35 @@
+//Kalkulator Sederhana
+
+#include <stdio.h>
+
+int main() {
+	double a,b;
+	char pilihan;
+	
+	printf("Input jenis operasi yang akan dilakukan (+, -, /, *)\n");
+	scanf("%c", &pilihan);
+
+	printf("Input angka 1: ");
+	scanf("%lf", &a);
+
+	printf("Input angka 2: ");
+	scanf("%lf", &b);
+	
+	switch(pilihan){
+		case '+':
+			printf("%.lf", a+b);
+			break;
+		case '-':
+			printf("%.lf", a-b);
+			break;
+		case '*':
+			printf("%.lf", a*b);
+			break;
+		case '/':
+			printf("%.2lf", a/b);
+			break;
+		default:
+			printf("Masukkan input yang valid!");
+			break;
+	}
+}
