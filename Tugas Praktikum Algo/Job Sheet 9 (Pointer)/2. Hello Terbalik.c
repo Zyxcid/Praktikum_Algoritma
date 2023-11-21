bb62dd66da8181aff5382d2566a6b4cb43646110
Zyxcid <121550192+Zyxcid@users.noreply.github.com>
2023-11-21 08:03:31 +0700

    Add files via upload

---
+++ Tugas Praktikum Algo/Job Sheet 9 (Pointer)/2. Hello Terbalik.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int i;
+    char aisatsu[6] = {'H', 'E', 'L', 'L', 'O'};
+    
+    char *ptr_aisatsu = aisatsu;
+    
+    //  Untuk output HELLO
+    for (i = 0; i < 5; i++) {
+        printf("%c", ptr_aisatsu[i]);
+    }
+    printf("\n");
+    //  Untuk output terbalik / OLLEH:
+	for (i = 4; i >= 0; i--) {
+        printf("%c", ptr_aisatsu[i]);
+    }
+    
+	return 0;
+}
+
---
+++ Tugas Praktikum Algo/Job Sheet 9 (Pointer)/3. Lesley, Layla dan Balmond.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int lesley = 57082, layla, balmond;
+	
+    printf("Untuk pertanyaan A\n");
+    layla = lesley;
+    balmond = layla + 1;
+    //  Nilai Lesley
+    printf("Nilai Layla = %d\n", layla);
+    //  Nilai Balmond
+    printf("Nilai Balmond = %d\n\n", balmond);
+	
+	
+    printf("Untuk pertanyaan B\n");
+	int *ptr_layla = &lesley;
+    balmond = *ptr_layla + 1;
+	//  Nilai Layla
+    printf("Nilai Layla = %d\n", *ptr_layla);
+    //  Nilai Balmond
+    printf("Nilai Balmond = %d\n", balmond);
+	
+    return 0;
+}
+
